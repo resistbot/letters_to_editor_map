a681c3efbc3b89362883d7429dfb13d95d142a97
chelseatroy <chelsea.dommert@gmail.com>
2017-12-05 21:32:37 -0500

    We now save off any zipcodes we have already geocoded to minimize our use of the geocoder API per data formatting run.

--- data.py
+++ data.py
@@ -2,11 +2,18 @@ import pandas as pd
 from pygeocoder import Geocoder
 import ast
 import time
+import json
 
 input_data = pd.read_csv('data.csv')
 
-input_data['ZIP'] = input_data['ZIP'].astype(str).str.zfill(5)
+extant_zipcodes = {}
+if 'zipcodes.json':
+    with open('zipcodes.json', 'r') as f:
+        stored_zipcodes = json.load(f)
+if stored_zipcodes != None:
+    extant_zipcodes = stored_zipcodes
 
+input_data['ZIP'] = input_data['ZIP'].astype(str).str.zfill(5)
 grouped = (input_data.groupby(['ZIP']).apply(lambda x: x.to_json(orient='records')))
 
 map_items = []
@@ -15,20 +22,36 @@ for key, value in grouped.iteritems():
     item = {}
     item['zipcode'] = key
     print key
-    item['latitude'] = Geocoder.geocode(key).latitude
+
+    try:
+        existing_info = extant_zipcodes[key]
+        print "Getting zipcode location from storage..."
+        item['latitude'] = existing_info['latitude']
+        item['longitude'] = existing_info['longitude']
+    except:
+        print "Fetching zipcode location from geocoder..."
+        item['latitude'] = Geocoder.geocode(key).latitude
+        item['longitude'] = Geocoder.geocode(key).longitude
+        print "Storing zipcode location for future use..."
+        new_location = {}
+        new_location['latitude'] = item['latitude']
+        new_location['longitude'] = item['longitude']
+        extant_zipcodes[key] = new_location
+        index += 1
+
+        if index % 5 == 0:
+            print "Waiting for geocode api..."
+            time.sleep(2)
     print item['latitude']
-    item['longitude'] = Geocoder.geocode(key).longitude
     print item['longitude']
-    if index % 5 == 0:
-        print "Waiting for geocode api..."
-        time.sleep(5)
-    item['letters'] = ast.literal_eval(value) #gets them to a list
+
+    item['letters'] = ast.literal_eval(value)  # gets them to a list
     map_items.append(item)
-    index += 1
 
 output_data = {}
 output_data['map_items'] = map_items
 
-import json
 with open('data.json', 'w') as fp:
     json.dump(output_data, fp)
+with open('zipcodes.json', 'w') as fp:
+    json.dump(extant_zipcodes, fp)
