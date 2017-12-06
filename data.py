cb15172a3bc64ff96b418a0316e04426fc2dbb76
chelseatroy <chelsea.dommert@gmail.com>
2017-12-05 20:19:25 -0500

    Group data by latitude and longitude
    - Fix zip codes that start with zero cutting off

--- data.py
+++ data.py
@@ -3,9 +3,11 @@ from pygeocoder import Geocoder
 
 data = pd.read_csv('data.csv')
 
+data['ZIP'] = data['ZIP'].astype(str).str.zfill(5)
+
 data['latitude'] = Geocoder.geocode(data["ZIP"]).latitude
 data['longitude'] = Geocoder.geocode(data["ZIP"]).longitude
 
-# grouped = data.groupby(['ZIP']).size()
-grouped = (data.groupby(['ZIP']).apply(lambda x: x.to_json(orient='records'))).to_json('smang')
-print grouped
+grouped = (data.groupby(['ZIP', 'latitude', 'longitude'])
+           .apply(lambda x: x.to_json(orient='records'))
+           ).to_json('data.json')
